ac31cb625f21bc925d9e6e6bc7182e125c9abece
dhergert <david@hergert.us>
2018-07-31 22:52:27 -0400

    adding some gitpassport stuff, alias, ansible config

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,18 +2,25 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+#git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
+		--exclude ".vscode/" \
+		--exclude "git/" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
+	rsync -avh --no-perms ./git/* ~/.git;
+	# move install to mac-dev-playbook project
+	#ln -sf ~/.git/hooks/bin/git-passport/git-passport.py ./00-git-passport
 	source ~/.bash_profile;
 }
+
+chmod u+x ~/bin/*
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
